839514567a241aead1881354ee07340050103dfc
Doug Mello <dmello88@gmail.com>
2019-08-02 22:58:13 -0400

    added setup.py file

---
+++ gmail.py
@@ -0,0 +1,1 @@
+Class gmail_api:
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dal',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Doug',
+    author_email='',
+    description=''
+)
